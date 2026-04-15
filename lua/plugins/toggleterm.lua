266d0dff7b9dfd6e1b311c55d364185db777de4e
PongTon420 <tututien123@gmail.com>
2026-04-15 10:30:30 +0700

    xoa keybind cua toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -11,7 +11,7 @@ return {
             insert_mappings = true,
             terminal_mappings = true,
             persist_size = true,
-            direction = "horizontal", -- can be 'vertical', 'float', or 'tab'
+            direction = "horizontal", -- 'vertical', 'float', or 'tab'
             close_on_exit = true,
             shell = vim.o.shell,
             float_opts = {
@@ -19,20 +19,5 @@ return {
                 winblend = 3,
             },
         })
-
-        -- Custom terminal keys
-        function _G.set_terminal_keymaps()
-            local opts = {buffer = 0}
-            vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-            vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-            vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-            vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-            vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-            vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-            vim.keymap.set('n', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-            vim.keymap.set('n', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-            vim.keymap.set('n', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-            vim.keymap.set('n', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-        end
     end
 }
